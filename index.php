ddf810ec1dce0085de49e2b85dd979a17f1cfa50
MonzerOmer1234 <monzeromer048@gmail.com>
2024-05-29 14:24:31 +0200

    defining shuffling in php

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $nums = [1, 2, 3, 4, 5, 6];
+// $nums_keys = array_keys($nums);
+// $new_nums = [];
+// $final_keys = [];
+// for($i = 0 ; $i < count($nums) ; $i++){
+//     $key = array_rand($nums_keys , 1); 
+//      $new_key = array_splice($nums_keys , $key , 1);
+//      array_push($final_keys , $new_key[0]);
+//   } ;
+// for($i = 0 ; $i < count($final_keys); $i++){
+//     $new_nums[$i] = $nums[$final_keys[$i]];
+// };
+// echo '<pre>';
+// print_r($new_nums);
+// echo'</pre>';
+
+///////////////////////////////////////////////////////////////////////////////////////
+// $url1 = "http://www.elz ero.org";
+// $url2 = "http://¥elzero.org";
+// $url3 = "https://elzero.org";
+// $url4 = "https://elzero.o¥rg";
+
+
+// $urls = [$url1 , $url2 , $url3 , $url4];
+
+// for($i = 0 ; $i < count($urls) ; $i++){
+//     if(filter_var($urls[$i] , FILTER_VALIDATE_URL)){
+//         echo $urls[$i] . '<br>';
+//     } else{
+//         echo filter_var($urls[$i] , FILTER_SANITIZE_URL) . '<br>';
+//     }
+// }
+
+

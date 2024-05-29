f9a466a44ac32de29d830decf26fc78ebb3b8d54
MonzerOmer1234 <monzeromer048@gmail.com>
2024-05-29 14:33:21 +0200

    fix

--- index.php
+++ index.php
@@ -1,58 +1,19 @@
 <?php
-
-
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $nums = [1, 2, 3, 4, 5, 6];
-// $nums_keys = array_keys($nums);
-// $new_nums = [];
-// $final_keys = [];
-// for($i = 0 ; $i < count($nums) ; $i++){
-//     $key = array_rand($nums_keys , 1); 
-//      $new_key = array_splice($nums_keys , $key , 1);
-//      array_push($final_keys , $new_key[0]);
-//   } ;
-// for($i = 0 ; $i < count($final_keys); $i++){
-//     $new_nums[$i] = $nums[$final_keys[$i]];
-// };
-// echo '<pre>';
-// print_r($new_nums);
-// echo'</pre>';
+$nums = [1, 2, 3, 4, 5, 6];
+$nums_keys = array_keys($nums);
+$new_nums = [];
+$final_keys = [];
+for($i = 0 ; $i < count($nums) ; $i++){
+    $key = array_rand($nums_keys , 1); 
+     $new_key = array_splice($nums_keys , $key , 1);
+     array_push($final_keys , $new_key[0]);
+  } ;
+for($i = 0 ; $i < count($final_keys); $i++){
+    $new_nums[$i] = $nums[$final_keys[$i]];
+};
+echo '<pre>';
+print_r($new_nums);
+echo'</pre>';
 
 ///////////////////////////////////////////////////////////////////////////////////////
 // $url1 = "http://www.elz ero.org";
